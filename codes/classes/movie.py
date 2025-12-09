65d41d6d4d32dcf6c7f11c0885a5c01e97d26805
AlsornaJP <jpribeirojacob@gmail.com>
2025-12-09 18:23:21 -0300

    questão 5 finalizada com a criação das classes movie e series

---
+++ codes/classes/movie.py
@@ -0,0 +1,9 @@
+from TV import TV
+
+class movie(TV):
+    def __init__(self, title, year, rating):
+        super().__init__(title, year)
+        self.rating = rating
+    
+    def __str__(self):
+        return f"{self.title}({self.year}) - Nota: {self.rating}"
